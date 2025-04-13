184fd8cf2cb18d1f82b53a8aff756edb36ed5c04
FardeenQamar <qamarfardeen@gmail.com>
2025-04-13 18:17:46 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,11 @@
+import math
+
+number = int(input('Enter an integer :  '))
+sqr = math.sqrt(number)
+log_e = math.log(number)
+sin = math.sin(number)
+
+print(f'Square root : {sqr}')
+print(f'log e : {log_e}')
+print(f'Sin : {sin}')
+
